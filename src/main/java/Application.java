06548d3fcc200619fb935efd9f0cff2cdefa3551
unknown <nik.ignition.work@gmail.com>
2019-06-19 17:23:17 +0300

    final version, some code commented for demonstrating all work of programm but it should be uncommented for correct long-term work of programm

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,33 +1,39 @@
 import domain.CardStatus;
 import domain.ClientCard;
+import sensor.Sensor;
+import sensor.SensorEventManager;
+import sensor.SensorNotificationListener;
 import turnstile.Turnstile;
+
 import java.math.BigDecimal;
 import java.util.Date;
 
 public class Application {
 
-  public static void main(String[] args) {
-    //invalid card -- reason blocked
-    ClientCard clientCard = new ClientCard(1, new BigDecimal("1234"), new Date().getTime() + 500000, CardStatus.BLOCKED);
-    //invalid card -- reason outdated
-    ClientCard clientCard1 = new ClientCard(2, new BigDecimal("1234"), new Date().getTime() - 500000, CardStatus.WORKING);
-    //invalid card -- reason not enough money;
-    ClientCard clientCard2 = new ClientCard(3, new BigDecimal("2"), new Date().getTime() + 500000, CardStatus.WORKING);
+    public static void main(String[] args) {
+        //invalid card -- reason blocked
+        ClientCard clientCard = new ClientCard(1, new BigDecimal("1234"), new Date().getTime() + 500000, CardStatus.BLOCKED);
+        //invalid card -- reason outdated
+        ClientCard clientCard1 = new ClientCard(2, new BigDecimal("1234"), new Date().getTime() - 500000, CardStatus.WORKING);
+        //invalid card -- reason not enough money;
+        ClientCard clientCard2 = new ClientCard(3, new BigDecimal("2"), new Date().getTime() + 500000, CardStatus.WORKING);
 
-    //valid card
-    ClientCard clientCard3 = new ClientCard(4, new BigDecimal("100"), new Date().getTime() + 500000, CardStatus.WORKING);
+        //valid card
+        ClientCard clientCard3 = new ClientCard(4, new BigDecimal("100"), new Date().getTime() + 500000, CardStatus.WORKING);
 
-    Turnstile turnstile = new Turnstile();
-    turnstile.setClientCard(clientCard);
-    turnstile.passPassanger();
+        Turnstile turnstile = new Turnstile();
+        turnstile.setClientCard(clientCard);
+        turnstile.passPassanger();
 
-    turnstile.setClientCard(clientCard1);
-    turnstile.passPassanger();
+        turnstile.setClientCard(clientCard1);
+        turnstile.passPassanger();
 
-    turnstile.setClientCard(clientCard2);
-    turnstile.passPassanger();
+        turnstile.setClientCard(clientCard2);
+        turnstile.passPassanger();
 
-    turnstile.setClientCard(clientCard3);
-    turnstile.passPassanger();
-  }
+        turnstile.setClientCard(clientCard3);
+        turnstile.passPassanger();
+
+
+    }
 }
--- src/main/java/database/Writer.java
+++ src/main/java/database/Writer.java
@@ -10,26 +10,26 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 public class Writer {
-  private ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
-  private Long midnight = LocalDateTime.now().until(LocalDate.now().plusDays(1).atStartOfDay(), ChronoUnit.MINUTES);
-  private static final String INFO_FILE_PATH= "src/main/resources/InMemoryDatabase.txt";
+    private ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+    private Long midnight = LocalDateTime.now().until(LocalDate.now().plusDays(1).atStartOfDay(), ChronoUnit.MINUTES);
+    private static final String INFO_FILE_PATH = "src/main/resources/InMemoryDatabase.txt";
 
-  public Writer() {
-   // executorService.scheduleAtFixedRate(this::sendFileSomewhereFarFarAway, midnight, TimeUnit.DAYS.toMinutes(1), TimeUnit.MINUTES);
-  }
-
-  public void writeToDatabase(String info) {
-    try (BufferedWriter writer = new BufferedWriter(new FileWriter(INFO_FILE_PATH, true))) {
-      writer.append("\n");
-      writer.write(info);
-      writer.close();
-    } catch (IOException ex) {
-      System.out.println(ex.getMessage());
+    public Writer() {
+        // executorService.scheduleAtFixedRate(this::sendFileSomewhereFarFarAway, midnight, TimeUnit.DAYS.toMinutes(1), TimeUnit.MINUTES);
     }
-  }
+
+    public void writeToDatabase(int id, long timestamp) {
+        String info = "Card with id " + id + " was passed in timestamp = " + timestamp;
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(INFO_FILE_PATH, true))) {
+            writer.append("\n");
+            writer.write(info);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 
 
-  private void sendFileSomewhereFarFarAway() {
-    //don't have address =(
-  }
+    private void sendFileSomewhereFarFarAway() {
+        //don't have address =(
+    }
 }
